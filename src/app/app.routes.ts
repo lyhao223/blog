5204f1c19d51ef62078cc07b8f1c8a1336ba441d
Ly Hao <lyhao0710@gmail.com>
2024-09-25 20:02:23 +0700

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { FormCreatePostComponent } from './form-create-post/form-create-post.component';
+import { AlbumComponent } from './album/album.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'blog-list', title: 'Blog list', component: BlogListComponent },
+  {
+    path: 'create-the-blog',
+    title: 'Create the blog',
+    component: FormCreatePostComponent,
+  },
+  { path: 'album', title: 'Album', component: AlbumComponent },
+];
